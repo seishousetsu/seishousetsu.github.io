f94b3d1345ee53e7657799cd4e497b2f0e1064cb
sjkim04 <sjkim04@users.noreply.github.com>
2022-02-08 04:10:29 +0000

    deploy: 391883f1f28e4fd4efab724d8eca710f9af07ba1

--- pass.js
+++
@@ -1,9 +0,0 @@
-function passcheck(){
-	if ($('.pass').val() == "031-12") { // ansとinputTxtが同じだったら
-		window.location.href("031-12.html"); // 指定したサイトに飛ぶ
-	} else { // それ以外は
-		$(".pass").val(""); //文字列を消し
-		$('.pass').attr('placeholder', '❌'); //違うという❌を表示
-	}
-});
-};
