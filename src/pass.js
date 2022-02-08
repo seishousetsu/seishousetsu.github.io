812e7c001379684596efb0e7b6d301afbaf66fc6
sjk <38371178+sjkim04@users.noreply.github.com>
2022-02-08 12:11:06 +0900

    Update pass.js

--- src/pass.js
+++ src/pass.js
@@ -1,14 +1,16 @@
 var w = $(window).width();
 var ans = '031-12';
+var inputTxt;
 $('button').on('click', function() {
-			if (w >= 767) {
-				var inputTxt = $(this).prev('.pass').val();
-			} else {
-				var inputTxt = $(this).parent().prev('.pass').val();
-			}
-			if (inputTxt.toLowerCase() === ans.toLowerCase()) {
-				setTimeout("location.href='031-12test.html';", 0);
-			} else {
-				$(".pass").val("");
-				$('.pass').attr('placeholder', '❌');
-			}});
+	if (w >= 767) {
+		inputTxt = $(this).prev('.pass').val();
+	} else {
+		inputTxt = $(this).parent().prev('.pass').val();
+	}
+	if (inputTxt.toLowerCase() === ans.toLowerCase()) {
+		window.location.href("031-12.html");
+	} else {
+		$(".pass").val("");
+		$('.pass').attr('placeholder', '❌');
+	}
+});
