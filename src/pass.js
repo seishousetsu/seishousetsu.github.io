21d4b029f0fb2be05f189e026c9dd8062481d2cb
sjk <38371178+sjkim04@users.noreply.github.com>
2022-02-08 12:38:00 +0900

    Update pass.js

--- src/pass.js
+++ src/pass.js
@@ -1,12 +1,11 @@
-var w = $(window).width();
-var ans = '031-12';
-var inputTxt = $('.pass').val();
+var ans = '031-12'; //入力した文字列と一致させたいもの。この場合は031-12
+var inputTxt = $('.pass').val(); //これが入力した文字列。これと031-12が同じだったら違うサイトに飛ばせたい
 function passcheck(){
-	if (inputTxt === ans) {
-		window.location.href("031-12.html");
-	} else {
-		$(".pass").val("");
-		$('.pass').attr('placeholder', '❌');
+	if (inputTxt === ans) { // ansとinputTxtが同じだったら
+		window.location.href("031-12.html"); // 指定したサイトに飛ぶ
+	} else { // それ以外は
+		$(".pass").val(""); //文字列を消し
+		$('.pass').attr('placeholder', '❌'); //違うという❌を表示
 	}
 });
-}
+};
