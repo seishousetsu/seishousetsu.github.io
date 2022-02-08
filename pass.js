e5f2c18fb61cba5fe2093dcd3809f31f5ed5221f
sjkim04 <sjkim04@users.noreply.github.com>
2022-02-08 03:30:24 +0000

    deploy: d814ea50e0d00f351c17c9d085bd1975e350fe52

--- pass.js
+++ pass.js
@@ -1,14 +1,8 @@
 var w = $(window).width();
 var ans = '031-12';
-var inputTxt;
+var inputTxt = $('.pass').val();
 function passcheck(){
-	$('.button').on('click', function() {
-	if (w >= 767) {
-		inputTxt = $('.pass').val();
-	} else {
-		inputTxt = $('.pass').val();
-	}
-	if (inputTxt() === ans()) {
+	if (inputTxt === ans) {
 		window.location.href("031-12.html");
 	} else {
 		$(".pass").val("");
